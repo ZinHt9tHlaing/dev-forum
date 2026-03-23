b30bf00dadccd52947e284146981eab163d41706
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-03-24 00:38:35 +0630

    link components

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { FAKE_POSTS } from "@/data";
+import Link from "next/link";
+
 interface SinglePostPageProps {
   params: Promise<{ id: string }>;
 }
@@ -5,7 +8,21 @@ interface SinglePostPageProps {
 const SinglePostPage = async ({ params }: SinglePostPageProps) => {
   const { id } = await params;
 
-  return <div>SinglePostPage {id}</div>;
+  const post = FAKE_POSTS.find((post) => post.id === parseInt(id));
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">{post.title}</h2>
+      <Link href="/" className="underline text-blue-500">Back to Home</Link>
+      <p className="text-sm font-medium text-gray-500 line-clamp-1">
+        {post.body}
+      </p>
+    </div>
+  );
 };
 
 export default SinglePostPage;
