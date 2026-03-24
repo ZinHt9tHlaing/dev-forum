f696708cd83f8d3cb7490ecbb92194bcbe69efc0
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-03-24 11:49:48 +0630

    path constant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
 import { FAKE_POSTS } from "@/data";
+import { PATHS } from "@/path";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h2>Home Page</h2>
-      <Link href="/posts">Posts</Link>
+      <div className="flex gap-2 underline text-blue-500">
+        <Link href={PATHS.ABOUT}>About</Link>
+        <Link href={PATHS.POSTS}>Posts</Link>
+      </div>
 
       <div>
         {FAKE_POSTS.map((post) => (
           <div key={post.id} className="p-4 border-b border-gray-200">
             <h3 className="text-xl font-bold">{post.title}</h3>
-            <p className="text-sm font-medium text-gray-500 line-clamp-1">{post.body}</p>
-            <Link href={`/posts/${post.id}`} className="underline text-blue-500">
+            <p className="text-sm font-medium text-gray-500 line-clamp-1">
+              {post.body}
+            </p>
+            <Link
+              href={PATHS.SINGLE_POST(post.id.toString())}
+              className="underline text-blue-500"
+            >
               Read More
             </Link>
           </div>
---
+++ src/path.ts
@@ -0,0 +1,7 @@
+export const PATHS = {
+  ABOUT: "/about",
+  CONTACT: "/contact",
+  HOME: "/",
+  POSTS: "/posts",
+  SINGLE_POST: (id: string) => `${PATHS.POSTS}/${id}`,
+};
